e8f09e55857def61882016c1b00d2d830b6aad46
Zac Holland <zacharyholland@gmail.com>
2019-03-10 19:36:45 -0700

    create agent sidebar tool works

---
+++ components/createAgent.js
@@ -0,0 +1,149 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { Grid, Typography, Paper, TextField, Button } from "@material-ui/core";
+
+const {
+  CreateAgentRequest,
+  Entity
+} = require("../pkg/api/v1/simulation-service_pb");
+const {
+  SimulationServiceClient
+} = require("../pkg/api/v1/simulation-service_grpc_web_pb");
+
+const API_VERSION = "v1";
+
+const styles = theme => ({
+  paper: {
+    padding: 10
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  }
+});
+
+class CreateAgent extends React.Component {
+  state = {
+    x: 0,
+    y: 0
+  };
+
+  componentDidMount() {
+    this.simService = new SimulationServiceClient(
+      "http://127.0.0.1:9091",
+      null,
+      null
+    );
+  }
+
+  createAgent = async () => {
+    const { firebase } = this.props;
+    const { x, y } = this.state;
+
+    // Get current user's auth token
+    firebase
+      .auth()
+      .currentUser.getIdToken()
+      .then(token => {
+        // Create the new agent
+        var agent = new Entity();
+        agent.setX(x);
+        agent.setY(y);
+        var request = new CreateAgentRequest();
+        request.setApi(API_VERSION);
+        request.setAgent(agent);
+        var metadata = { "auth-token": token };
+        const call = this.simService.createAgent(
+          request,
+          metadata,
+          (err, resp) => {
+            console.log("Sub response: ", resp);
+          }
+        );
+        call.on("status", status => {
+          console.log("Create agent status: ", status);
+        });
+      })
+      .catch(err => {
+        console.error("Error refreshing id token", err);
+        return false;
+      });
+  };
+
+  // Create a new agent with a testing
+  createAgent_test = async () => {
+    const { x, y } = this.state;
+    // Create the new agent
+    var agent = new Entity();
+    agent.setX(x);
+    agent.setY(y);
+    var request = new CreateAgentRequest();
+    request.setApi(API_VERSION);
+    request.setAgent(agent);
+    var metadata = { "auth-token": "TEST-ID-TOKEN" };
+    const call = this.simService.createAgent(request, metadata, (err, resp) => {
+      console.log("Sub response: ", resp);
+    });
+    call.on("status", status => {
+      console.log("Create agent status: ", status);
+    });
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Typography>
+            <b>Create Agent</b>
+          </Typography>
+          <TextField
+            id="x"
+            label="X Position"
+            value={this.state.x}
+            onChange={this.handleChange("x")}
+            type="number"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true
+            }}
+            margin="normal"
+          />
+          <TextField
+            id="x"
+            label="Y Position"
+            value={this.state.y}
+            onChange={this.handleChange("y")}
+            type="number"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true
+            }}
+            margin="normal"
+          />
+          <Button color="primary" onClick={this.createAgent_test}>
+            Create
+          </Button>
+        </Paper>
+      </Grid>
+    );
+  }
+}
+
+CreateAgent.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default compose(
+  withStyles(styles),
+  connect(state => {
+    console.log(state);
+  })
+)(CreateAgent);
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import uuidv1 from "uuid/v1";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import Spectate from "../components/spectate";
+import CreateAgent from "../components/createAgent";
 import withNavBar from "../lib/withNavBar";
 import { withFirebase, firebaseConnect } from "react-redux-firebase";
 
@@ -15,31 +16,17 @@ const styles = ({ palette, spacing, breakpoints }) => ({
   },
   cellInspectorContainer: {
     padding: 10
+  },
+  centerText: {
+    textAlign: "center"
   }
 });
 
-const {
-  CreateAgentRequest,
-  CreateSpectatorRequest,
-  SubscribeSpectatorToRegionRequest,
-  Region,
-  Agent
-} = require("../pkg/api/v1/simulation-service_pb");
 const {
   SimulationServiceClient
 } = require("../pkg/api/v1/simulation-service_grpc_web_pb");
 
 const API_VERSION = "v1";
-
-const CELLS_IN_REGION = 10;
-const CELL_SIZE = 20;
-const WORLD_CENTER_OFFSET = CELLS_IN_REGION * CELL_SIZE;
-const CANVAS_SIZE = CELLS_IN_REGION * 2 * CELL_SIZE;
-
-const LEFT_KEY_CODE = 37;
-const RIGHT_KEY_CODE = 39;
-const UP_KEY_CODE = 38;
-const DOWN_KEY_CODE = 40;
 
 class Index extends React.Component {
   state = {
@@ -47,47 +34,9 @@ class Index extends React.Component {
   };
 
   async componentDidMount() {
-    // Initialize service
-    var simService = new SimulationServiceClient(
-      "http://127.0.0.1:9091",
-      null,
-      null
-    );
-    this.simService = simService;
-
     // Add key listener
     document.addEventListener("keydown", this._handleKeyDown);
   }
-
-  createAgent = async () => {
-    const { firebase } = this.props;
-    const { simService } = this;
-    const TEST_POS = 0;
-    // Get current user's auth token
-    firebase
-      .auth()
-      .currentUser.getIdToken()
-      .then(token => {
-        // Create the new agent
-        var agent = new Agent();
-        agent.setX(TEST_POS);
-        agent.setY(TEST_POS);
-        var request = new CreateAgentRequest();
-        request.setApi(API_VERSION);
-        request.setAgent(agent);
-        var metadata = { "auth-token": token };
-        const call = simService.createAgent(request, metadata, (err, resp) => {
-          console.log("Sub response: ", resp);
-        });
-        call.on("status", status => {
-          console.log("Create agent status: ", status);
-        });
-      })
-      .catch(err => {
-        console.error("Error refreshing id token", err);
-        return false;
-      });
-  };
 
   // When an entity is clicked in the spectate window,
   //  add it's data to the side bar
@@ -103,25 +52,34 @@ class Index extends React.Component {
     return (
       <div>
         <Grid container spacing={32} justify="center">
-          <Grid item xs={12}>
-            <Typography variant="h4">Hello Twitch!</Typography>
+          <Grid item xs={12} className={classes.centerText}>
+            <Typography variant="h4">
+              The worlds first shared, online, persistant AI environment
+            </Typography>
           </Grid>
+          {/* Spectate Window */}
           <Grid item>
             <Spectate onEntityClick={this.onEntityClick} />
           </Grid>
+          {/* Side bar */}
           <Grid item xs={3}>
-            <Paper className={classes.cellInspectorContainer}>
-              <Typography>
-                <b>Cell Inspector</b>
-              </Typography>
-              {selectedEntity ? (
-                <Typography>
-                  <b>Id: </b> {selectedEntity.id}
-                  <br />
-                  <b>Class: </b> {selectedEntity.class}
-                </Typography>
-              ) : null}
-            </Paper>
+            <Grid container spacing={16} direction="column">
+              <Grid item xs={12}>
+                <Paper className={classes.cellInspectorContainer}>
+                  <Typography>
+                    <b>Cell Inspector</b>
+                  </Typography>
+                  {selectedEntity ? (
+                    <Typography>
+                      <b>Id: </b> {selectedEntity.id}
+                      <br />
+                      <b>Class: </b> {selectedEntity.class}
+                    </Typography>
+                  ) : null}
+                </Paper>
+              </Grid>
+              <CreateAgent />
+            </Grid>
           </Grid>
         </Grid>
 
