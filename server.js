4de95ddf1165db787b06f85636c0b1c19abe9f7c
Zac Holland <zacharyholland@gmail.com>
2019-04-13 15:43:17 -0600

    force https

--- server.js
+++ server.js
@@ -6,16 +6,30 @@ const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+function ensureSecure(req, res, next) {
+  if (req.secure) {
+    // OK, continue
+    return next();
+  }
+  // handle port numbers if you need non defaults
+  // res.redirect('https://' + req.host + req.url); // express 3.x
+  res.redirect("https://" + req.hostname + req.url); // express 4.x
+}
+
 app
   .prepare()
   .then(() => {
-    const server = express();
+    const app = express();
 
-    server.get("*", (req, res) => {
+    if (process.env.NODE_ENV == "production") {
+      app.all("*", ensureSecure);
+    }
+
+    app.get("*", (req, res) => {
       return handle(req, res);
     });
 
-    server.listen(port, err => {
+    app.listen(port, err => {
       if (err) throw err;
       console.log("> Ready on http://localhost:3000");
     });
