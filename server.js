80458a62da393119b8cf6967cb45bc85ff5a5d03
Zac Holland <zacharyholland@gmail.com>
2019-04-13 15:57:19 -0600

    fixed forcehttps

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const next = require("next");
+var enforce = require("express-sslify");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-function ensureSecure(req, res, next) {
-  if (req.secure) {
-    // OK, continue
-    return next();
-  }
-  // handle port numbers if you need non defaults
-  // res.redirect('https://' + req.host + req.url); // express 3.x
-  res.redirect("https://" + req.hostname + req.url); // express 4.x
-}
-
 app
   .prepare()
   .then(() => {
     const app = express();
 
-    if (process.env.NODE_ENV == "production") {
-      app.all("*", ensureSecure);
+    if (process.env.NODE_ENV === "production") {
+      app.use(enforce.HTTPS());
     }
 
     app.get("*", (req, res) => {
