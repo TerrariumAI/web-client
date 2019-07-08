58b66600ad11c4cd2f0a30077f800e4796339e15
Zac Holland <zacharyholland@gmail.com>
2019-07-08 16:30:35 -0700

    attempting to add react-redux-firebase

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Layout from "../components/MyLayout.js";
 import Link from "next/link";
 import { EnvironmentClient } from "../api/environment_grpc_web_pb";
 import { GetEntityRequest, Entity } from "../api/environment_pb";
-// import fetch from "isomorphic-unfetch";
-import { db } from "../lib/db";
 
 const addr = "localhost:9091";
 
@@ -13,25 +11,7 @@ class Index extends React.Component {
     this.state = { loading: true, sf: {} };
   }
 
-  componentDidMount() {
-    console.log(db);
-    var docRef = db.collection("cities").doc("SF");
-
-    docRef
-      .get()
-      .then(function(doc) {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          this.setState(() => ({ loading: false, sf }));
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      })
-      .catch(function(error) {
-        console.log("Error getting document:", error);
-      });
-  }
+  componentDidMount() {}
 
   // componentDidMount() {
   //   var service = new EnvironmentClient(addr, null, null);
