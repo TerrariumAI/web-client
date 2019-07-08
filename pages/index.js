1276b828877096cab440b01a1f03a24b9e832059
Zac Holland <zacharyholland@gmail.com>
2019-07-08 16:23:25 -0700

    attempt at adding firebase

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,48 @@ import Link from "next/link";
 import { EnvironmentClient } from "../api/environment_grpc_web_pb";
 import { GetEntityRequest, Entity } from "../api/environment_pb";
 // import fetch from "isomorphic-unfetch";
+import { db } from "../lib/db";
 
 const addr = "localhost:9091";
+
 class Index extends React.Component {
-  componentDidMount() {
-    var service = new EnvironmentClient(addr, null, null);
-    var request = new GetEntityRequest();
-    request.setId("0");
-    var metadata = {};
-    service.getEntity(request, metadata, (err, resp) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log(resp);
-    });
+  constructor() {
+    super();
+    this.state = { loading: true, sf: {} };
   }
+
+  componentDidMount() {
+    console.log(db);
+    var docRef = db.collection("cities").doc("SF");
+
+    docRef
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          this.setState(() => ({ loading: false, sf }));
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+  }
+
+  // componentDidMount() {
+  //   var service = new EnvironmentClient(addr, null, null);
+  //   var request = new GetEntityRequest();
+  //   request.setId("0");
+  //   var metadata = {};
+  //   service.getEntity(request, metadata, (err, resp) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     console.log(resp);
+  //   });
+  // }
 
   render() {
     return (
