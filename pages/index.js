e4ec6de48fa6b78e568808882e333a99f5019fd5
Zac Holland <zacharyholland@gmail.com>
2019-07-08 17:03:33 -0700

    implemented react-redux-firebase

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
-import Layout from "../components/MyLayout.js";
-import Link from "next/link";
+import React from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
 import { EnvironmentClient } from "../api/environment_grpc_web_pb";
 import { GetEntityRequest, Entity } from "../api/environment_pb";
+import { withFirebase, firestoreConnect } from "react-redux-firebase";
 
 const addr = "localhost:9091";
 
 class Index extends React.Component {
-  constructor() {
-    super();
-    this.state = { loading: true, sf: {} };
-  }
-
-  componentDidMount() {}
-
   // componentDidMount() {
   //   var service = new EnvironmentClient(addr, null, null);
   //   var request = new GetEntityRequest();
@@ -27,12 +22,15 @@ class Index extends React.Component {
   // }
 
   render() {
-    return (
-      <Layout>
-        <h1>Batman TV Shows</h1>
-      </Layout>
-    );
+    const { users } = this.props;
+    console.log(users);
+    return <div>sdf</div>;
   }
 }
-
-export default Index;
+export default compose(
+  withFirebase,
+  firestoreConnect(["users"]), // or { collection: 'todos' }
+  connect((state, props) => ({
+    users: state.firestore.ordered.users
+  }))
+)(Index);
