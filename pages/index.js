f99424afbcf371294eb2789753cc372903a63e4b
Zac Holland <zacharyholland@gmail.com>
2019-08-08 10:09:16 -0700

    added code snippets on the home page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,100 @@ import React from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import Link from "next/link";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+let dark = {
+  "hljs": {
+    "display": "block",
+    "overflowX": "auto",
+    "padding": "0.5em",
+    "background": "#444",
+    "color": "#ddd"
+  },
+  "hljs-keyword": {
+    "color": "white",
+    "fontWeight": "bold"
+  },
+  "hljs-selector-tag": {
+    "color": "white",
+    "fontWeight": "bold"
+  },
+  "hljs-literal": {
+    "color": "white",
+    "fontWeight": "bold"
+  },
+  "hljs-section": {
+    "color": "white",
+    "fontWeight": "bold"
+  },
+  "hljs-link": {
+    "color": "white"
+  },
+  "hljs-subst": {
+    "color": "#ddd"
+  },
+  "hljs-string": {
+    "color": "#d88"
+  },
+  "hljs-title": {
+    "color": "#d88",
+    "fontWeight": "bold"
+  },
+  "hljs-name": {
+    "color": "#d88",
+    "fontWeight": "bold"
+  },
+  "hljs-type": {
+    "color": "#d88",
+    "fontWeight": "bold"
+  },
+  "hljs-attribute": {
+    "color": "#d88"
+  },
+  "hljs-symbol": {
+    "color": "#d88"
+  },
+  "hljs-bullet": {
+    "color": "#d88"
+  },
+  "hljs-built_in": {
+    "color": "#d88"
+  },
+  "hljs-addition": {
+    "color": "#d88"
+  },
+  "hljs-variable": {
+    "color": "#d88"
+  },
+  "hljs-template-tag": {
+    "color": "#d88"
+  },
+  "hljs-template-variable": {
+    "color": "#d88"
+  },
+  "hljs-comment": {
+    "color": "#777"
+  },
+  "hljs-quote": {
+    "color": "#777"
+  },
+  "hljs-deletion": {
+    "color": "#777"
+  },
+  "hljs-meta": {
+    "color": "#777"
+  },
+  "hljs-doctag": {
+    "fontWeight": "bold"
+  },
+  "hljs-strong": {
+    "fontWeight": "bold"
+  },
+  "hljs-emphasis": {
+    "fontStyle": "italic"
+  }
+};
+
 import { withFirebase, firestoreConnect } from "react-redux-firebase";
 import {
   Typography,
@@ -15,6 +109,7 @@ import {
 import withNavbar from "../src/withNavbar";
 import SimpleEnvObs from "../components/simpleEnvObs";
 import { makeStyles } from "@material-ui/core/styles";
+
 
 const useStyles = makeStyles(theme => ({
   header: {
@@ -95,6 +190,94 @@ const Index = ({ users }) => {
       </Grid>
 
       <Container className={classes.infoCardGrid} maxWidth="md">
+        <Typography variant="h2" align="center" gutterBottom>Getting started in Python</Typography>
+        <Typography variant="h5" color="textSecondary" gutterBottom paragraph>
+          Our goal is to be <i>as simple as possible.</i> 
+          <br />
+          <br />
+          Once you have created a Remote Model in your dashboard, simply install our pip package:
+          <SyntaxHighlighter language='python' style={dark}>{"pip install terrariumai"}</SyntaxHighlighter>
+          Create a simple model function
+          <SyntaxHighlighter language='python' style={dark}>{modelStr}</SyntaxHighlighter>
+          Connect to your remote model
+          <SyntaxHighlighter language='python' style={dark}>{connectModelStr}</SyntaxHighlighter>
+          It's that easy!
+          <br />
+          <br />
+          Now you can interract with the live environment from the dashboard. The agents you spawn will
+          be using the code running on your local machine!
+        </Typography>
+      </Container>
+
+      <Container className={classes.infoCardGrid} maxWidth="md">
+        <Grid container>
+          <Grid item xs={12} >
+            <Typography variant="h2" align="center" gutterBottom>Train Your Model</Typography>
+            <Typography variant="h5" align="center" color="textSecondary" gutterBottom paragraph>
+              You will most likely want to train your model before connecting it to our live environment.
+              This can be done with our training environment that runs locally on your computer. It is kept
+              up to date with the live server and has 0 delay.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Download
+                </Typography>
+                <Typography>
+                  Download the training environment from our git repo.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Download
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Run the server
+                </Typography>
+                <Typography>
+                  Run the server on your local machine
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Run
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Connect
+                </Typography>
+                <Typography>
+                  Connect the same way you would to the live servers
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Connect
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Container className={classes.infoCardGrid} maxWidth="md">
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={4}>
             <Card className={classes.card}>
@@ -162,3 +345,24 @@ export default compose(
   //   users: state.firestore.ordered.users
   // }))
 )(Index);
+
+
+const modelStr = `import random
+from terrariumai import connectRemoteModel, createAction
+
+def randomAction():
+    return random.randint(0, 1)
+def randomDirection():
+    return random.randint(0, 3)
+
+def modelFunc(obsv):
+  if obsv.isAlive:
+    action = createAction(obsv.id, randomAction(), randomDirection())
+    return action
+  if not obsv.isAlive:
+    print("Entity died :(")`
+
+const connectModelStr = `connectRemoteModel(
+  secret='<model-secret>', 
+  modelFunc=modelFunc
+)`
