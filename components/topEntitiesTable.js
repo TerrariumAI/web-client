2f6951543c0ddea66bf96aeb268b585578f3f7f3
Zac Holland <zacharyholland@gmail.com>
2019-09-03 16:28:04 -0700

    added no entities table value

--- components/topEntitiesTable.js
+++ components/topEntitiesTable.js
@@ -1,57 +1,95 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import { withFirebase, withFirestore, firestoreConnect, isLoaded, isEmpty, populate } from "react-redux-firebase";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { Table, TableHead, TableCell, TableBody, TableRow, Paper, CircularProgress } from "@material-ui/core";
 
-const populates = [{ child: 'ModelID', root: 'remoteModels' }]
+const populates = [{ child: 'model', root: 'remoteModels' }]
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   spacer: {
     flexGrow: 1
   },
   divider: {
     borderLeft: '0.1em solid white', padding: '0.5em'
   }
-}));
+});
 
-let TopEntitiesTable = ({ firebase, auth, entities }) => {
-  const classes = useStyles();
-  if (!isLoaded(entities)) {
-    return <CircularProgress></CircularProgress>
+class TopEntitiesTable extends React.Component {
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
   }
-  if (isEmpty(entities)) {
-    return null // TODO
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
-  console.log(entities)
-  return (
-    <Paper>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Model</TableCell>
-            <TableCell align="right">Age</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.keys(entities).map(id => {
-            const entity = entities[id]
-            return (<TableRow key={id}>
-              <TableCell component="th" scope="row">
-                {id}
-              </TableCell>
-              <TableCell align="right">{entity.ModelID.name}</TableCell>
-              <TableCell align="right">{entity.CreatedAt}</TableCell>
-            </TableRow>)
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  )
+
+  render() {
+    const { classes, entities } = this.props;
+    if (!isLoaded(entities)) {
+      return <CircularProgress></CircularProgress>
+    }
+    if (isEmpty(entities)) {
+      return (
+        <Paper>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Model</TableCell>
+              <TableCell align="right">Age</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow key={"no-agents"}>
+                <TableCell component="th" scope="row">
+                  <i>No agents alive at right now</i>
+                </TableCell>
+              </TableRow>
+          </TableBody>
+          </Table>
+        </Paper>
+        
+      ) // TODO
+    }
+    return (
+      <Paper>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Model</TableCell>
+              <TableCell align="right">Age</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.keys(entities).map(id => {
+              const entity = entities[id]
+              const createdAt = entity.createdAt.toDate()
+              const now = new Date()
+              let delta = Math.floor((now - (createdAt) )/1000)
+              const hours = Math.floor(delta / 3600) % 24
+              delta -= hours * 3600
+              const minutes = Math.floor(delta/60) % 60;
+              delta -= minutes * 60
+              const seconds = delta % 60
+              return (<TableRow key={id}>
+                <TableCell component="th" scope="row">
+                  {id}
+                </TableCell>
+                <TableCell align="right">{entity.model.name || ""}</TableCell>
+                <TableCell align="right">{hours}:{minutes}:{seconds}</TableCell>
+              </TableRow>)
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    )
+  }
 }
 
 export default compose(
+  withStyles(styles),
   withFirebase,
   withFirestore,
   connect(({ firestore, firebase: { auth, profile } }, props) => ({
@@ -63,7 +101,7 @@ export default compose(
     {
       collection: "entities",
       orderBy: [
-        ['CreatedAt', 'asc']
+        ['createdAt', 'asc']
       ],
       limit: 10,
       populates,
